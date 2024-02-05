d88af5330e2701a021536fe11eaa45f8cc3d710d
Rabindra Khadka <rabindra.khadka@91signage.com>
2024-02-05 17:31:24 +0545

    releasing version code 4 and version name 1.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.borkor.shobizandoid"
         minSdk = 21
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.2"
+        versionCode = 4
+        versionName = "1.3"
         vectorDrawables {
             useSupportLibrary = true
         }
